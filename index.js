8c0193aae73d24cb6bf83d0061bf496a577edcb4
Partha1999 <impartha1999@gmail.com>
2022-12-23 01:08:00 +0600

    backend connected 2

--- index.js
+++ index.js
@@ -212,7 +212,12 @@ async function run() {
       }
     });
 
-    
+    app.delete("/allProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await allProductsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
